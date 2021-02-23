25fb0dff4c85faff03ef205aadf8c3c9491c63d9
arunprasad-07 <arunprasad.azhagesan@gmail.com>
2021-02-23 09:17:52 +0530

    Add project files.

---
+++ GenericSamples.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced
+{
+    public class GenericSamples
+    {
+
+    }
+
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+            Console.WriteLine("Genric Add");
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    /// <summary>
+    /// Types of constraints in Generics.
+    /// ---------------------------------
+    /// 1. where T : IComparable
+    /// 2. where T : Products
+    /// 3. where T : struct
+    /// 4. where T : class
+    /// 5. where T : new()
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public class Utilities<T> where T : IComparable
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public T Max(T a, T b)  
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        //public T Max<T>(T a, T b) where T : IComparable
+        //{
+        //    return a.CompareTo(b) > 0 ? a : b;
+        //}
+    }
+
+    public class Nullable<T> where T : struct
+    {
+        public Nullable(T value)
+        {
+
+        }
+
+
+        public T Value { get; set; }
+    }
+
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+            Console.WriteLine("Generic Dictionary Add method");
+        }
+    }
+
+    public class ObjectList
+    {
+        public void Add(object value)
+        {
+            Console.WriteLine("Object add");
+        }
+
+        public object this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            Console.WriteLine("Book Add");
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSharpAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic Explanations / Samples
+            GenericExplanation();
+
+            Console.ReadKey();
+        }
+        public static void GenericExplanation()
+        {
+            Book book = new Book { BookId = 1, BookName = "C#Book", AuthorName = "C#Author" };
+
+            //var bookList = new BookList();
+            //bookList.Add(book);
+
+            //// If we use the object class will cause us BOXING and UNBOXING performance penalty
+            ///-----------------------------------------------------------------------------------
+            //var objectList = new ObjectList();
+            //objectList.Add(book);
+
+            //// From Add method intellisense we clearly see that the ADD method is of type INT we declared above
+            //////-----------------------------------------------------------------------------------------------
+            //var genericList = new GenericList<int>();
+            //genericList.Add(5);
+
+            //var genericBookList = new GenericList<Book>();
+            //genericBookList.Add(book);
+
+            // From the below namespace there are in-built generic classes available in .net
+            ///-----------------------------------------------------------------------------
+            //System.Collections.Generic
+
+            //var genericDictionary = new GenericDictionary<string, Book>();
+            //genericDictionary.Add("KeyString", book);
+
+            // Constraints in Generics
+            //Utilities utilities = new Utilities();
+            //Console.WriteLine(utilities.Max<int>(7, 6));
+
+            Utilities<int> utilities = new Utilities<int>();
+            Console.WriteLine(utilities.Max(7, 8));
+
+        
+       }
+    }
+}
